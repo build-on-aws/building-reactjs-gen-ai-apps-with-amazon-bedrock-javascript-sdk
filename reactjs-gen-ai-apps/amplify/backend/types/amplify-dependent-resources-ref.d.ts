4a2d69c4bfa7d776afee0532939e4a0e4761add3
Enrique <enrique.rodriguez.garrido@gmail.com>
2024-05-06 23:29:42 -0400

    fix

---
+++ reactjs-gen-ai-apps/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "buildingreactjsgenai": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "llmchatbotjs6f7265086f726508": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
