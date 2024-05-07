19ee8c610f1b2274dd1a0b510c6e5cd4cda1dfe3
Enrique <enrique.rodriguez.garrido@gmail.com>
2024-05-06 22:03:07 -0400

    arreglo problema de newprompt undefined

--- reactjs-gen-ai-apps/src/PromptNew.jsx
+++ reactjs-gen-ai-apps/src/PromptNew.jsx
@@ -3,7 +3,7 @@ import BasicNewForm from "./BasicNewForm"
 import {useRef } from 'react';
 import { useNavigate } from "react-router-dom";
 
-import {PromptCreateForm}   from './ui-components';
+import PromptCreateForm   from './ui-components/PromptCreateFormV2';
 
 //todo: fix new prompt ID
 
@@ -12,6 +12,7 @@ export default () => {
 
     const childRef = useRef(null);
     const onSuccess = (e) => {
+        console.log(e)
         childRef.current.showMessage(e, "success", "Success", "Created in Database!")
         navigate(`/prompt/${e.id}`)
     }
